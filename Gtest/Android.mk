67fac44261838b6f88afa0c0acf14b24d3259912
GongPu <pu.gong@intel.com>
2023-04-26 17:26:43 +0800

    Use source file to build
    Signed-off-by: GongPu <pu.gong@intel.com>

--- Gtest/Android.mk
+++ Gtest/Android.mk
@@ -6,20 +6,25 @@ LOCAL_CFLAGS += -Wno-unused-parameter -Wno-macro-redefined -fexceptions
 
 LOCAL_MODULE_TAGS:= optional
 
-LOCAL_SRC_FILES := main.cpp sensor_client.cpp
-LOCAL_SHARED_LIBRARIES += liblog libcutils libutils libsock_util
+LOCAL_SHARED_LIBRARIES += liblog libcutils libutils
 LOCAL_MULTILIB := 64
 LOCAL_VENDOR_MODULE := true
 LOCAL_STATIC_LIBRARIES += libgtest_main libgtest libgmock
 
 LOCAL_CPPFLAGS := $(LOG_FLAGS) $(WARNING_LEVEL) $(DEBUG_FLAGS) $(VERSION_FLAGS)
 
-
 LOCAL_C_INCLUDES += \
     $(LOCAL_PATH) \
     $(LOCAL_PATH)/../ \
     $(LOCAL_PATH)/../sock_utils/include \
     $(LOCAL_PATH)/../../../external/googletest/googletest/include
+
+LOCAL_SRC_FILES := \
+    main.cpp \
+    sensor_client.cpp \
+    $(LOCAL_PATH)/../sock_utils/sock_utils.cpp \
+    $(LOCAL_PATH)/../sock_utils/sock_client.cpp \
+    $(LOCAL_PATH)/../sock_utils/sock_server.cpp
 
 LOCAL_MODULE:= SensorTest
 
