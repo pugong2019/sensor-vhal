878b09f5b090e1b6b4ae94bc3841d6042d6709eb
GongPu <pu.gong@intel.com>
2023-04-27 10:08:57 +0800

    Fix redifinition issue
    Signed-off-by: GongPu <pu.gong@intel.com>

--- Gtest/SensorFixture.cpp
+++ Gtest/SensorFixture.cpp
@@ -16,7 +16,7 @@ TEST_F(SensorFixture, SensorsNumberCheck)
     ASSERT_EQ(SUPPORTED_SENSORS_NUMBER, m_sensor_client.get_sensor_num());
 }
 
-TEST_F(SensorFixture, AccelerometerEnabledCheck)
+TEST_F(SensorFixture, AccelerometerDefaultEnabledCheck)
 {
     this_thread::sleep_for(std::chrono::microseconds(1000));
     ASSERT_TRUE(m_sensor_client.is_acc_default_enabled());
